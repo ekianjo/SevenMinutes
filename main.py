e9dca8c4acd1e9e7e2be94fd80b2ae7f9297b3b3
ekianjo <ekianjo@gmail.com>
2013-08-02 11:29:29 +0900

    Create main.py
    key of seven minutes program

---
+++ main.py
@@ -0,0 +1,12 @@
+#1. Jumping jacks Total body 
+#2. Wall sit Lower body 
+#3. Push-up Upper body 
+#4. Abdominal crunch Core 
+#5. Step-up onto chair Total body 
+#6. Squat Lower body 
+#7. Triceps dip on chair Upper body 
+#8. Plank Core 
+#9. High knees/running in place Total body 
+#10. Lunge Lower body 
+#11. Push-up and rotation Upper body 
+#12. Side plank Core
