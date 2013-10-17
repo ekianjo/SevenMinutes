a95c1b9ae595e307b797340227bda3426c9dade0
ekianjo <ekianjo@gmail.com>
2013-10-17 14:37:19 +0900

    Update main.py
    updated

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 
-
 #Check again how to open Cairo context
 #get pictures for each item, if possible with 2 steps animations.
 #CHeck again how to make timer (easy)
 #get voice over / Glados style ? 
 #this is really a weekend project, should be able to finish this in less than 10 hours. 
+
+#other ideas
+#confirm calendar day when done -> and if user has been active recently or not
+#combine with wii-u weight measurement ? :)
+
 
 
 #1. Jumping jacks Total body 
